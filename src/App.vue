439fb81ddb058fdb906c57f1a8653f4af5240278
ChingisD <chingisbd@gmail.com>
2025-05-05 20:47:50 -0500

    added DB and configured API Gateway

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
-    <amplify-authenticator>
-      <AppNavbar />
-      <HeroSelection />
-      <AppFooter />
-    </amplify-authenticator>
-  </div>
+  <Authenticator>
+    <template v-slot="{ signOut }">
+      <div id="app">
+        <AppNavbar />
+        <HeroSelection />
+        <button @click="signOut" style="margin: 20px;">Sign Out</button>
+        <button @click="createUser">Test API - Create User</button>
+        <AppFooter />
+      </div>
+    </template>
+  </Authenticator>
 </template>
 
 <script>
-import { components } from '@aws-amplify/ui-vue';
+import { Authenticator } from '@aws-amplify/ui-vue';
 import AppNavbar from './components/AppNavbar.vue';
 import HeroSelection from './components/HeroSelection.vue';
 import AppFooter from './components/AppFooter.vue';
@@ -17,14 +21,33 @@ import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'App',
   components: {
-    ...components, // Important to register Amplify components
+    Authenticator,
     AppNavbar,
     HeroSelection,
     AppFooter
+  },
+  methods: {
+    async createUser() {
+      try {
+        const response = await fetch('https://4wy085db51.execute-api.us-east-1.amazonaws.com/Dev/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: 'user001',
+            name: 'John Doe',
+            email: 'john@example.com'
+          })
+        });
+        const data = await response.json();
+        console.log(data);
+        alert('User created!');
+      } catch (err) {
+        console.error(err);
+        alert('Error creating user.');
+      }
+    }
   }
 };
 </script>
-
-<style>
-/* Add some global styles if needed */
-</style>
